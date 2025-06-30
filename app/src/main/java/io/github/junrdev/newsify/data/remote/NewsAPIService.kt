31415e3f8147731da0c8b94e2da47dca425ed397
briank <98683954+kdbrian@users.noreply.github.com>
2025-06-30 21:32:52 +0300

    Update NewsAPIService.kt

--- app/src/main/java/io/github/junrdev/newsify/data/remote/NewsAPIService.kt
+++ app/src/main/java/io/github/junrdev/newsify/data/remote/NewsAPIService.kt
@@ -6,19 +6,22 @@ import io.github.junrdev.newsify.util.ApiKeyProvider
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
+import io.github.junrdev.newsify.BuildConfig
+
+
 
 interface NewsAPIService {
 
     @GET("everything")
     suspend fun getNewsBySearchString(
         @Query("q") query: String,
-        @Query("apiKey") apiKey: String = ApiKeyProvider.API_KEY
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY
     ): Response<NewsResponse>
 
     @GET("top-headlines")
     suspend fun getTopHeadlines(
         @Query("country") country: String = "US".lowercase(),
-        @Query("apiKey") apiKey: String = ApiKeyProvider.API_KEY
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY
     ): Response<NewsResponse>
 
 }
